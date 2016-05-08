ff17e4dc6075ae16e0eefd38ba06727dbd40bc9a
Julien Perret <julien.perret@gmail.com>
2016-05-08 22:45:25 +0200

    correction in file handling

--- src/main/java/org/thema/mupcity/MupCityTask.java
+++ src/main/java/org/thema/mupcity/MupCityTask.java
@@ -9,23 +9,18 @@ public class MupCityTask {
 	public static void run(long seed, File data, File results) throws IOException, SchemaException {
 		String name = "testExplo";
 		File dir = results;
-		File buildFile = new File(data + "BATI_AU.shp");
+		File buildFile = new File(data, "BATI_AU.shp");
 		int exp = 3;
 		double minSize = 20;
 		double maxSize = 25273;
 		boolean useNoBuild = true;
 		boolean network = true;// true => network distance
-		File roadFile = new File(data + "route_sans_chemin.shp");
-		File facilityFile = new File(data + "CS_au_besac_sirene_2012.shp");
-		File leisureFile = new File(data + "loisirs.shp");
-		File busFile = new File(data + "stations_besac_tram_2015.shp");
-		File trainFile = new File(data + "gare_train_ICONE_docs_2015.shp");
-		File restrictFile = new File(data + "ICONE-zonesNU_AU.shp");
-		double seuilDensBuild = 0.0;// NO PARAMETER FOR THAT
-		boolean memedos = true;// si l'on veut ou non une organisation dans des
-								// dossiers déocupant les scénario, ou
-								// uniquement les grilles
-
+		File roadFile = new File(data, "route_sans_chemin.shp");
+		File facilityFile = new File(data, "CS_au_besac_sirene_2012.shp");
+		File leisureFile = new File(data, "loisirs.shp");
+		File busFile = new File(data, "stations_besac_tram_2015.shp");
+		File trainFile = new File(data, "gare_train_ICONE_docs_2015.shp");
+		File restrictFile = new File(data, "ICONE-zonesNU_AU.shp");
 		// definition de la grille
 		double width = 32243;
 		double height = 33602;
